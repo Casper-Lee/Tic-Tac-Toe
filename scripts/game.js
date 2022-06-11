56c8e6b655056b606aaab9f2fda1335cefa175df
Casper Lee <casperino@caspers-air.lan>
2022-06-11 15:09:56 +0800

    start of game requirements before continue with the game

---
+++ scripts/game.js
@@ -0,0 +1,11 @@
+function startNewGame(){
+    if(players[0].name === '' || players[1].name === ''){
+        alert('Please set custom player names for both players!')
+        return;
+    }
+    gameAreaElement.style.display = 'block'
+}
+
+function selectGameField(event){
+    event.target.text = 
+}
